655595f0d3784912b0cfec6ce3db9588c3772656
Sara Misri <sara.misri.sm@gmail.com>
2026-04-07 10:21:21 +0300

    fixed a path in __tests__

--- src/components/__tests__/Homescreen.test.tsx
+++ src/components/__tests__/Homescreen.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import HomeScreen from '../../Screen/HomeScreen';
+import HomeScreen from '../../Screens/HomeScreen';
 
 describe('HomeScreen', () => {
   it('renders placeholder text', () => {
